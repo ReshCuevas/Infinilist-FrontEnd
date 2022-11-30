480e1a5e30c8c230f3ddd92d44c9b04eab9c3a62
luisdelamoraf <luise_delamora@hotmail.com>
2022-11-30 10:47:07 -0600

    get lists and singlelist

--- src/app/pages/playlist/playlist.component.ts
+++ src/app/pages/playlist/playlist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-playlist',
@@ -7,8 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaylistComponent implements OnInit {
 
-  constructor() { }
+  listas:any =[]
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    let xhr = new XMLHttpRequest;
+    var self = this
+    xhr.open('GET', environment.apiURL + '/api/getList/'+ localStorage.getItem('currentUser'));
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send();
+    xhr.onload = function(){
+      console.log(xhr.response)
+      self.listas = JSON.parse(xhr.response)
+    }
+  }
+  
+  saveListId(listId: any){
+    localStorage.setItem('listId', listId)
+    this.router.navigate(['singleslist'])
   }
 }
--- src/app/pages/singleslist/singleslist.component.ts
+++ src/app/pages/singleslist/singleslist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-singleslist',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleslistComponent implements OnInit {
 
+  lista: any = []
+
   constructor() { }
 
   ngOnInit(): void {
+    let xhr = new XMLHttpRequest;
+    		xhr.open('GET', environment.apiURL + '/api/getSingle/'+ localStorage.getItem('listId'));
+    		xhr.setRequestHeader('Content-Type','application/json');
+    		xhr.send();
+        var self = this
+    		xhr.onload = function(){
+        		self.lista = JSON.parse(xhr.response);
+        		console.log(self.lista);
+    		}
   }
-
 }
