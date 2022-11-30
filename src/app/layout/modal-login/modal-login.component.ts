7b12b484c8aad020ee008584f93829f4099b1536
luisdelamoraf <luise_delamora@hotmail.com>
2022-11-29 19:52:13 -0600

    socket client

--- src/app/layout/modal-login/modal-login.component.ts
+++ src/app/layout/modal-login/modal-login.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faKey, faPerson, faUser } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import * as socketIo from 'socket.io-client';
+
 
 @Component({
   selector: 'app-modal-login',
   templateUrl: './modal-login.component.html',
   styleUrls: ['./modal-login.component.sass']
 })
-export class ModalLoginComponent{
-  closeResult = '';
+export class ModalLoginComponent implements OnInit {
+
+  socketClient: any;
+
 
   constructor(private modalService: NgbModal, library: FaIconLibrary) {
     library.addIcons(
@@ -18,11 +22,17 @@ export class ModalLoginComponent{
     )
   }
 
+  ngOnInit(): void {
+    this.socketClient = socketIo.io('http://localhost:3000/')
+  }
+
 	open(content: any) {
 		this.modalService.open(content);
 	}
 
   login(){
+    this.socketClient.emit('login',{ id: 'usuario Demo'})
     this.modalService.dismissAll()
+    console.log('hola');
   }
 }
